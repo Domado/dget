e542652836a0394b69f21925e20ca30bb12fddfa
Domado <domado.chen@gmail.com>
2025-10-26 21:28:02 +0800

    Update daiso_net.h

--- daiso_net.h
+++ daiso_net.h
@@ -1,1 +1,12 @@
+#ifndef DAISO_NET_H
+#define DAISO_NET_H
 
+#include <stddef.h> 
+
+int daiso_net_connect(const char *host, const char *port);
+
+int daiso_net_send(int sockfd, const char *data, size_t len);
+
+int daiso_net_recv_dynamic(int sockfd, char **buffer_out);
+
+#endif
